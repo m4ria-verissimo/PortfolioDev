71fcfda4f29040f1400974c95ba042d49d83162e
Maria Eduarda Verissimo <duardaverissimo@gmail.com>
2026-07-16 16:30:01 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
+
+emailjs.init("5EfHEhw_veKyUtXjX");
+
 function abrirmodal(){
   const modal=  document.getElementById("modal-contato").style.display="flex";
 
   document.getElementById("fecharcntt").onclick=function(){
   document.getElementById("modal-contato").style.display="none";
   }
+}
+function enviar(){
+    
+   
+   const nome=  document.getElementById("nome").value;
+   const email = document.getElementById("email").value;
+   const msg = document.getElementById("msg").value;
+   
+   emailjs.send("service_bblmnym" , "template_6yrg249"{
+    nome: nome, 
+    email: email ,
+    mensagem: mensagem 
+    })
+    .then(()=> {
+        alert("ok");
+    });
+
+    
+
 }
