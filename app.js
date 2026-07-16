44381d6fab56a14cfe68e5fbf79939cdb5409235
Maria Eduarda Verissimo <duardaverissimo@gmail.com>
2026-07-16 14:59:53 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 function abrirmodal(){
   const modal=  document.getElementById("modal-contato").style.display="flex";
 
-
+  document.getElementById("fecharcntt").onclick=function(){
+  document.getElementById("modal-contato").style.display="none";
+  }
 }
